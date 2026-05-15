6956c035b1d889a1f41f720e19a8965a6b8e6025
Ueno <tenchanma@gmail.com>
2026-05-15 12:16:50 +0900

    Add Android overflow menu screen

---
+++ app/src/main/java/com/example/myapplication/OverflowMenuActivity.kt
@@ -0,0 +1,75 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+
+class OverflowMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_overflow_menu)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_item, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.timeline -> {
+                showDestinationPlaceholder(R.string.menu_timeline_title)
+                true
+            }
+
+            R.id.search -> {
+                showDestinationPlaceholder(R.string.menu_search_title)
+                true
+            }
+
+            R.id.whisper -> {
+                showDestinationPlaceholder(R.string.menu_whisper_title)
+                true
+            }
+
+            R.id.myprofile -> {
+                showDestinationPlaceholder(R.string.menu_profile_title)
+                true
+            }
+
+            R.id.profileedit -> {
+                showDestinationPlaceholder(R.string.menu_profile_edit_title)
+                true
+            }
+
+            R.id.logout -> {
+                Toast.makeText(this, getString(R.string.menu_logout_message), Toast.LENGTH_SHORT).show()
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showDestinationPlaceholder(titleResId: Int) {
+        val message = getString(R.string.menu_navigation_placeholder, getString(titleResId))
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
